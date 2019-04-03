0f5c88f6a69dbe0500b75fb42f025707ef8e7c8a
larionovdmitri <dlarionov@productiveedge.com>
2019-04-03 10:42:18 -0500

    chaned names

---
+++ boilerplate-with-npm-install/learning-center-generic-inc-vue/src/main.js
@@ -0,0 +1,34 @@
+import i18ninit from './i18n';
+import { createApp } from './createApp';
+
+/* eslint-disable no-underscore-dangle */
+
+/*
+  SSR Data
+  If we're running in a server-side rendering scenario,
+  the server will provide the window.__JSS_STATE__ object
+  for us to acquire the initial state to run with on the client.
+
+  This enables us to skip a network request to load up the layout data.
+
+  SSR is initiated from /server/server.js.
+*/
+
+// initialize the dictionary, then render the app
+// note: if not making a multlingual app, the dictionary init can be removed.
+i18ninit().then((i18n) => {
+  // HTML element to place the app into
+  const rootElement = document.getElementById('root');
+
+  // retrieve the initial app state if it is defined
+  let __JSS_STATE__ = null;
+  const ssrRawJson = document.getElementById('__JSS_STATE__');
+  if (ssrRawJson) {
+    __JSS_STATE__ = JSON.parse(ssrRawJson.innerHTML);
+  }
+
+  const initialState = __JSS_STATE__ || null;
+
+  const { app } = createApp(initialState, i18n);
+  app.$mount(rootElement);
+});
