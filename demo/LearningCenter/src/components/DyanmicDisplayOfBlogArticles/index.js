c334a3ccf126e03fe85ca4c21e6b22d5f39b1f61
larionovdmitri <dlarionov@productiveedge.com>
2019-07-17 15:40:19 -0500

    set up graph ql calls to set state in jss connected mode

--- demo/LearningCenter/src/components/DyanmicDisplayOfBlogArticles/index.js
+++ demo/LearningCenter/src/components/DyanmicDisplayOfBlogArticles/index.js
@@ -28,7 +28,7 @@ class DyanmicDisplayOfBlogArticles extends React.Component {
   }
 
   componentDidMount = () => {
-    const { graphQLEndpointPath, sitecoreApiKey } = config;
+    const { graphQLEndpointPath, sitecoreApiKey, sitecoreApiHost } = config;
     let graphQlQuery //may be for jss or for data
     //call graph ql endpoint to get a list of all articles under blog
     //check if running in jss start
@@ -45,33 +45,28 @@ class DyanmicDisplayOfBlogArticles extends React.Component {
     }
     if (!this.jssCodeFirstState) {
       axios({
-        url: `${graphQLEndpointPath}?sc_apikey=${sitecoreApiKey}`,
+        url: `${sitecoreApiHost}${graphQLEndpointPath}?sc_apikey=${sitecoreApiKey}`,
         method: "post",
         data: { query: graphQlQuery }
       })
         .then(response => {
-          debugger
-          let content = response.data;
           this.setState({
-            contentToDisplay: content
+            cardDataToRender: response.data.data
           });
         })
         .catch(err => {
-          debugger
           console.log(`
           GraphQL Error Getting Content Data:
           ${err}`);
         });
-      axios({
-        url: `${graphQLEndpointPath}?sc_apikey=${sitecoreApiKey}`,
-        method: "post",
-        data: { query: authorsQuery }
-      })
+        axios({
+          url: `${sitecoreApiHost}${graphQLEndpointPath}?sc_apikey=${sitecoreApiKey}`,
+          method: "post",
+          data: { query: authorsQuery }
+        })
         .then(response => {
-          debugger
-          let content = response.data;
           this.setState({
-            authorContent: content
+            authorContent: response.data.data
           });
         })
         .catch(err => {
@@ -122,11 +117,21 @@ class DyanmicDisplayOfBlogArticles extends React.Component {
               if(this.jssCodeFirstState){
                 imageSourceAltered = tileImage.src.replace('/sitecore/shell/-/media/learningcenter/', '').replace('.ashx', '.jpg');
               }else{
-                imageSourceAltered = tileImage.src.replace('/sitecore/shell/-/media/learningcenter/', '')
+                //since this sitecore instance is on another machine - provide the full path if in connected mode
+                imageSourceAltered = config.sitecoreApiHost + tileImage.src.replace('/sitecore/shell/-/media/learningcenter/', '')
               }
               
               blogCount += 1
-              let manipAuthorName = authorTag.value.replace('-', '')//since the name compes back with a dash
+              let manipAuthorName
+              let displayName
+              //temp handling of non set imported author tags
+              if(authorTag.value.indexOf("System" > -1)){
+                manipAuthorName = "DmitriLarionov"
+                displayName = "Dmitri Larionov"
+              }else{
+                manipAuthorName = authorTag.value.replace('-', '')//since the name compes back with a dash
+                displayName = authorTag.value.replace('-', ' ')
+              }
               if (this.lookUpOfAuthorUrl[manipAuthorName].id === this.state.authorValue || this.state.authorValue === 0) {
                 return (
                   <div className="blog-item" key={`${blogCount}blog`}>
@@ -141,7 +146,7 @@ class DyanmicDisplayOfBlogArticles extends React.Component {
                     </div>
                     <div className="blog-list-tileDescription">{tileDescription.value}</div>
                     <Link to={this.lookUpOfAuthorUrl[manipAuthorName].url}>
-                      <div className="blog-list-author">By {authorTag.value}</div>
+                      <div className="blog-list-author">By {displayName}</div>
                     </Link>
                   </div>
                 )
